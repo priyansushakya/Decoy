277c2a5349d6dab26afa593a8e803a6582ca210f
Priyansu Shakya <priyansushakya9@gmail.com>
2025-05-22 11:53:11 +0545

    Add files via upload

---
+++ src/main/webapp/DB/seed.sql
@@ -0,0 +1,78 @@
+INSERT INTO room (RoomNumber, RoomType, Price, Status, Description, Room_Image) VALUES
+('Room 1', 'Single', 2500, 'notBooked', '*🛏️ Cozy Retreat Near the Lake: Enjoy a peaceful stay in our well-furnished single room, just a short walk from the beautiful Phewa Lake. Perfect for solo travelers exploring the natural beauty of Pokhara.
+
+*🌄 Budget-Friendly Comfort: Our single room offers a clean, comfortable space with free Wi-Fi and a private bathroom — ideal for backpackers and budget travelers wanting easy access to Lakeside cafes and attractions.
+
+*🧘 Tranquil Escape for One: Nestled in the heart of Lakeside, this single room provides a quiet atmosphere, soft bedding, and mountain views — a great choice for solo adventurers or meditation seekers in Pokhara.
+', 'images/single1.jpg'),
+('Room 2', 'Single', 2500, 'notBooked', '*🛏️ Cozy Retreat Near the Lake: Enjoy a peaceful stay in our well-furnished single room, just a short walk from the beautiful Phewa Lake. Perfect for solo travelers exploring the natural beauty of Pokhara.
+
+*🌄 Budget-Friendly Comfort: Our single room offers a clean, comfortable space with free Wi-Fi and a private bathroom — ideal for backpackers and budget travelers wanting easy access to Lakeside cafes and attractions.
+
+*🧘 Tranquil Escape for One: Nestled in the heart of Lakeside, this single room provides a quiet atmosphere, soft bedding, and mountain views — a great choice for solo adventurers or meditation seekers in Pokhara.
+', 'images/single2.jpg'),
+('Room 3', 'Single', 2500, 'notBooked', '*🛏️ Cozy Retreat Near the Lake: Enjoy a peaceful stay in our well-furnished single room, just a short walk from the beautiful Phewa Lake. Perfect for solo travelers exploring the natural beauty of Pokhara.
+
+*🌄 Budget-Friendly Comfort: Our single room offers a clean, comfortable space with free Wi-Fi and a private bathroom — ideal for backpackers and budget travelers wanting easy access to Lakeside cafes and attractions.
+
+*🧘 Tranquil Escape for One: Nestled in the heart of Lakeside, this single room provides a quiet atmosphere, soft bedding, and mountain views — a great choice for solo adventurers or meditation seekers in Pokhara.
+', 'images/single3.jpg'),
+('Room 4', 'Deluxe', 5000, 'notBooked', '*🏞️ Lakeside Luxury with a View: Relax in our spacious deluxe room featuring modern furnishings, a queen-sized bed, and a balcony with stunning views of Phewa Lake and the Annapurna range.
+
+*🛋️ Elegant Comfort in the Heart of Pokhara: Our deluxe room offers a perfect blend of style and comfort, complete with air conditioning, a private bathroom, and close proximity to Lakeside’s vibrant restaurants and shops.
+
+*🌅 Scenic Stay for Elevated Comfort: Unwind in a deluxe room designed for relaxation, with plush interiors, premium amenities, and easy access to lakeside boating, trekking routes, and local attractions.
+', 'images/double1.jpg'),
+('Room 5', 'Deluxe', 5000, 'notBooked', '*🏞️ Lakeside Luxury with a View: Relax in our spacious deluxe room featuring modern furnishings, a queen-sized bed, and a balcony with stunning views of Phewa Lake and the Annapurna range.
+
+*🛋️ Elegant Comfort in the Heart of Pokhara: Our deluxe room offers a perfect blend of style and comfort, complete with air conditioning, a private bathroom, and close proximity to Lakeside’s vibrant restaurants and shops.
+
+*🌅 Scenic Stay for Elevated Comfort: Unwind in a deluxe room designed for relaxation, with plush interiors, premium amenities, and easy access to lakeside boating, trekking routes, and local attractions.
+', 'images/double2.jpg'),
+('Room 6', 'Deluxe', 5000, 'notBooked', '*🏞️ Lakeside Luxury with a View: Relax in our spacious deluxe room featuring modern furnishings, a queen-sized bed, and a balcony with stunning views of Phewa Lake and the Annapurna range.
+
+*🛋️ Elegant Comfort in the Heart of Pokhara: Our deluxe room offers a perfect blend of style and comfort, complete with air conditioning, a private bathroom, and close proximity to Lakeside’s vibrant restaurants and shops.
+
+*🌅 Scenic Stay for Elevated Comfort: Unwind in a deluxe room designed for relaxation, with plush interiors, premium amenities, and easy access to lakeside boating, trekking routes, and local attractions.
+', 'images/double3.jpg'),
+('Room 7', 'Suite', 10000, 'notBooked', '*🛋️ Spacious Lakeside Luxury: Experience top-tier comfort in our suite room, featuring separate living and sleeping areas, a king-sized bed, and a private balcony with breathtaking lake and mountain views.
+
+*🌄 Premium Escape in Pokhara: Perfect for families or couples, this suite offers refined elegance with modern décor, deluxe amenities, and walking-distance access to Phewa Lake and Lakeside attractions.
+
+*🏔️ Exclusive Comfort with a View: Indulge in a serene and stylish suite designed for a luxurious stay, complete with a mini lounge, en-suite bath, and panoramic views of the Annapurna and Machhapuchhre peaks.', 'images/suite1.jpg'),
+('Room 8', 'Suite', 10000, 'notBooked', '*🛋️ Spacious Lakeside Luxury: Experience top-tier comfort in our suite room, featuring separate living and sleeping areas, a king-sized bed, and a private balcony with breathtaking lake and mountain views.
+
+*🌄 Premium Escape in Pokhara: Perfect for families or couples, this suite offers refined elegance with modern décor, deluxe amenities, and walking-distance access to Phewa Lake and Lakeside attractions.
+
+*🏔️ Exclusive Comfort with a View: Indulge in a serene and stylish suite designed for a luxurious stay, complete with a mini lounge, en-suite bath, and panoramic views of the Annapurna and Machhapuchhre peaks.', 'images/suite2.jpg'),
+('Room 9', 'Suite', 10000, 'notBooked', '*🛋️ Spacious Lakeside Luxury: Experience top-tier comfort in our suite room, featuring separate living and sleeping areas, a king-sized bed, and a private balcony with breathtaking lake and mountain views.
+
+*🌄 Premium Escape in Pokhara: Perfect for families or couples, this suite offers refined elegance with modern décor, deluxe amenities, and walking-distance access to Phewa Lake and Lakeside attractions.
+
+*🏔️ Exclusive Comfort with a View: Indulge in a serene and stylish suite designed for a luxurious stay, complete with a mini lounge, en-suite bath, and panoramic views of the Annapurna and Machhapuchhre peaks.', 'images/suite3.jpg');
+
+
+INSERT INTO employee (
+    EmployeeID, FirstName, LastName, Username, Email, EmpPhone,
+    Password, Role, Salary, JoinedDate, Photo
+) VALUES 
+(2, 'hora', 'hora', 'ramhora', 'hora@gmail.com', '1234567891',
+ 'bYX+GrKEA...==', 'admin', 20000, '2025-05-15', 'Pages/Images/stunning.jpg'),
+
+(3, 'hora', 'hora', 'gurasmansini', 'hora1@gmail.com', '1234567891',
+ 'ldnpHlO7vN...==', 'receptionist', 20000, '2025-05-15', 'Pages/Images/stunning.jpg');
+
+INSERT INTO customer (
+    CustomerID, FirstName, LastName, Email, PhoneNumber,
+    NumberOfGuests, Requests, ImageStream
+) VALUES 
+(12, 'Salina', 'Gurung', 'Salina@gmail.com', '+1234567892', 2, 'nice', 'images/profile.jpg'),
+(13, 'Rhythm', 'Gurung', 'rhythm@gmail.com', '+1234567888', 4, 'beautiful', 'images/profile.jpg'),
+(14, 'Lucky', 'Gurung', 'lucky@gmail.com', '+1234567897', 3, 'Superb', 'images/profile.jpg'),
+(15, 'Sashant', 'Shrestha', 'sashant@gmail.com', '+1234567895', 2, 'awesome', 'images/profile.jpg'),
+(16, 'Prajwal', 'Gurung', 'prajwal@gmail.com', '+1234567896', 1, 'nice', 'images/profile.jpg'),
+(17, 'Selu', 'Gurung', 'selu@gmail.com', '+1234566542', 4, 'good', 'images/profile.jpg'),
+(18, 'Salina', 'Thapa', 'salina@gmail.com', '+1234567895', 2, 'nice', 'images/profile.jpg'),
+(19, 'Awantika', 'Gurung', 'awantika@gmail.com', '+1234567894', 1, 'hello', 'images/profile.jpg'),
+(20, 'Adam', 'Smith', 'adam@gmail.com', '+1234567893', 1, 'make it good', 'images/profile.jpg');
+
